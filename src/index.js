f3ca86d79b17e79e30bb19e351f8c45c3206bfa3
suraj <surajsuthar0067@gmail.com>
2024-08-26 08:49:48 +0530

    second

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 5002
 
 app.use(express.json())
 app.use("/api/v1",route)
+app.use("/",(req,res) =>{
+    res.send("It's on")
+})
 
 app.listen(PORT,()=>{
     console.log("server is on!")
